09e9b3105785a488d3026b18c29dea326182e816
MEdwards0 <matt.edwards21@protonmail.com>
2022-10-17 09:53:26 +0100

    Adding more logs to the app. displayUserHome being worked on.

--- src/viewHandler/routes/displayUserHome.js
+++ src/viewHandler/routes/displayUserHome.js
@@ -1,9 +1,12 @@
+const { log } = require("../../logging");
+
 const wrapper = controller => {
     return displayUserHome = async (req, res) => {
-
+            log(req).info({"session_id": req.session.id, "message": 'Go to /user-home.'});
             const user = req.session.class;
 
             if (user == undefined) {
+                log(req).info({ "session_id": req.session.id, "message": 'User undefined. Go to /.' });
                 res.redirect('/');
                 return;
             } else {
@@ -17,9 +20,15 @@ const wrapper = controller => {
                         userLevel: user.userLevel
                     };
 
+                    log(req).debug({ "session_id": req.session.id, "key_variables": {
+                        "userLevel": user.userLevel
+                    } });
+
                     const result = await controller.database.handleCheckToken(user.token, user.username);
 
                     user.removeCustomerInfo(); // Remove any customer information stored in the user class.
+
+                    result.status ? log(req).info({ "session_id": req.session.id, "message": 'Rendering user_home.' }) : log(req).info({ "session_id": req.session.id, "message": 'Token invalid. Go to /log-out.' });
 
                     result.status ? res.render('user_home', { profile: profile }) : res.redirect('/log-out');
 
